2e9e82f755d1dbcb967e442f1321ec8b8e690e83
Артем Лазарчик <lazarchik_2000@mail.ru>
2020-12-18 16:10:37 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Summa(int [,] mas)
+        {
+            int rows = mas.GetUpperBound(0) + 1;
+            int columns = mas.Length / rows;
+            int max = mas[0, 0];
+            int min = mas[0, 0];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (mas[i, i] > max) max = mas[i, i];
+                    if (mas[i, i] < min) min = mas[i, i];
+                }
+            }
+            return max + min;
+        }
+    }
+}
---
+++ library3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace library3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Введите кол-во строк матрицы: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите кол-во cтолбцов матрицы: ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    array[i, j] = rnd.Next(50) - 25;
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сумма минимального и максимального элементов главной диагонали: " + ClassLibrary1.Class1.Summa(array));
+            Console.ReadKey();
+        }
+    }
+}
